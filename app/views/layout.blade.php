f7432d0999a21242e5e36cfe565872ccdbdefd5d
Agraj-Prtp-Singh <singhagraj021@gmail.com>
2026-01-24 17:03:10 +0545

    Workshop8

---
+++ app/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+</head>
+<body>
+	<h2>Simple Employee CRUD</h2>
+	<hr>
+	@yield('content')
+</body>
+</html>
